f25750b8814782555b4baca1d87fd36a3d54a183
Kévin Lima <kevinlimaf20@gmail.com>
2024-05-30 17:36:49 -0100

    list-tasks.txt

--- tasks.py
+++ tasks.py
@@ -1,5 +1,21 @@
+def list_tasks():
+    for i, task in enumerate(tasks):
+        status = "Concluída" if task["completed"] else "Não Concluída"
+        print(f"{i + 1}. {task['task']} [{status}]")
+
 def main():
     print("Personal Task Manager")
+     while True:
+        command = input("Comando (add/list/exit): ")
+        if command == "add":
+            task = input("Tarefa: ")
+            add_task(task)
+        elif command == "list":
+            list_tasks()
+        elif command == "exit":
+            break
 
 if __name__ == "__main__":
     main()
+
+
